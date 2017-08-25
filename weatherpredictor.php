209e58f7811b8a09509e3d2d97f2193c0998d1cc
Remy <remy.e.francis@gmail.com>
2017-08-24 19:57:56 -0700

    Second Commit

--- weatherpredictor.php
+++ weatherpredictor.php
@@ -13,7 +13,6 @@
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
 <!--Optional theme-->
-
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
 <style>
